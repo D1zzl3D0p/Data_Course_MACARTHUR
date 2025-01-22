153095942793af1c2deb322a3090a1ec833ae582
D1zzl3D0p <spn1kolat3sla@gmail.com>
2025-01-22 16:00:37 -0700

    Submitting assignment 3

--- Assignments/Assignment_3/Assignment_3.R
+++ Assignments/Assignment_3/Assignment_3.R
@@ -22,7 +22,7 @@
 1:10 # ':' only works for integers
 letters # built-in pre-made vector of a - z
 
-
+class(letters)
 
 vector1 <- c(1,2,3,4,5,6,7,8,9,10)
 vector2 <- c(5,6,7,8,4,3,2,1,3,10)
@@ -45,11 +45,12 @@ letters == "a"
 letters != "c"
 letters %in% c("a","b","c","z")
 vector1 %in% 1:6
-
+!(vector1 %in% 1:6)
 
 # Data Frames ####
 # R has quite a few built-in data sets
 data("iris") # load it like this
+
 
 # For built-in data, there's often a 'help file'
 ?iris
@@ -109,6 +110,8 @@ class(nums) # make sure it's numeric
 as.factor(nums) # show in console
 nums_factor <- as.factor(nums) #assign it to a new object as a factor
 class(nums_factor) # check it
+nums_factor + 1 # this one will throw an error
+nums + 1
 
 
 # convert numeric to character
@@ -118,7 +121,7 @@ as.character(vector1) + 5
 # convert character to numeric
 as.numeric(vector3)
 
-
+stringi::stri_enc_toascii("asbcd")
 
 
 #check it out
@@ -157,24 +160,26 @@ dbinom(50,100,.5) # probability of getting exactly 50 heads out of 100 coin flip
 
 seq(2,150,2) # here's the code to get a list of the even numbers between 2 and 150
 
+iris[seq(2,150,2),]
 
 
 # 2.  Create a new object called iris_chr which is a copy of iris, except where every column is a character class
 
+iris_chr = as.character(iris)
 
 
 # 3.  Create a new numeric vector object named "Sepal.Area" which is the product of Sepal.Length and Sepal.Width
 
-
+Sepal.Area = iris$Sepal.Length * iris$Sepal.Width
 
 # 4.  Add Sepal.Area to the iris data frame as a new column
 
-
+iris$Sepal.Area = Sepal.Area
 
 # 5.  Create a new dataframe that is a subset of iris using only rows where Sepal.Area is greater than 20 
       # (name it big_area_iris)
 
-
+big_area_iris = iris[iris$Sepal.Area > 20,]
 
 # 6.  Upload the last numbered section of this R script (with all answers filled in and tasks completed) 
       # to canvas
